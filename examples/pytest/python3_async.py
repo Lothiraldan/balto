b29e3d873ffdbe501b3a301b9cfdac76e2389e5b
Boris Feld <lothiraldan@gmail.com>
2018-01-16 22:00:06 +0100

    Import the pytext examples into balto
    It would makes it easier to test balto for beginners and to run acceptance
    test on balto later.

---
+++ examples/pytest/python3_async.py
@@ -0,0 +1,39 @@
+import asyncio
+
+async def _read_stream(stream, cb):  
+    while True:
+        line = await stream.readline()
+        if line:
+            cb(line)
+        else:
+            break
+
+async def _stream_subprocess(cmd, stdout_cb, stderr_cb):  
+    process = await asyncio.create_subprocess_exec(*cmd,
+            stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+
+    await asyncio.wait([
+        _read_stream(process.stdout, stdout_cb),
+        _read_stream(process.stderr, stderr_cb)
+    ])
+    return await process.wait()
+
+
+def execute(cmd, stdout_cb, stderr_cb):  
+    loop = asyncio.get_event_loop()
+    rc = loop.run_until_complete(
+        _stream_subprocess(
+            cmd,
+            stdout_cb,
+            stderr_cb,
+    ))
+    loop.close()
+    return rc
+
+if __name__ == '__main__':  
+    print(execute(
+        ["pytest", "--json", "."],
+        lambda x: print("STDOUT: %s" % x),
+        lambda x: print("STDERR: %s" % x),
+    ))
+
---
+++ examples/pytest/test_slow.py
@@ -0,0 +1,6 @@
+import time
+
+def test_slow_passing():
+    time.sleep(2)
+
+    assert True
