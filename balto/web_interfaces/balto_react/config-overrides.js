90393eca9854c5a3a23586a24b0e2c904910c2e8
Boris Feld <lothiraldan@gmail.com>
2019-08-13 19:46:50 -0400

    Get rid of react-app-rewired

--- balto/web_interfaces/balto_react/config-overrides.js
+++
@@ -1,9 +0,0 @@
-const rewireReactHotLoader = require('react-app-rewire-hot-loader');
-const rewireEslint = require('react-app-rewire-eslint');
-
-/* config-overrides.js */
-module.exports = function override(config, env) {
-  config = rewireReactHotLoader(config, env);
-  config = rewireEslint(config, env);
-  return config;
-}
