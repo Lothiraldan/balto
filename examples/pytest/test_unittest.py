a8e2d1e08d2033a6beb31f7a26802e43699eed9c
Boris Feld <lothiraldan@gmail.com>
2019-08-21 23:09:05 +0200

    Ensure files in a directory works properly
    I'm about to refactor how tests are saved on the frontend in order to improve
    performance but first let's check that integrations data have this scenario.

---
+++ examples/pytest/test_unittest.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertTrue('Foo'.isupper())
+
+if __name__ == '__main__':
+    unittest.main()
+
