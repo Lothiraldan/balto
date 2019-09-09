b02d8db8254e65bd53e4694e3ccf0fcc7dcad148
Boris Feld <lothiraldan@gmail.com>
2019-09-09 22:33:00 +0200

    Add more information in Run details modal

--- balto/web_interfaces/balto_react/src/components/RunsList.js
+++ balto/web_interfaces/balto_react/src/components/RunsList.js
@@ -7,114 +7,117 @@ import Moment from "react-moment";
 
 import { convert } from "../time";
 
-class ReturnCode extends React.Component {
+class RunDetails extends React.PureComponent {
+  render() {
+    const { show, handle_close, run_id, done, test_number, status, return_message } = this.props;
+
+    return (
+      <Modal
+        closeOnBlur={true}
+        show={show}
+        showClose={false}
+        onClose={handle_close}
+      >
+        <Modal.Card style={{ display: 'flex', width: '90%' }}>
+          <Modal.Card.Head onClose={handle_close}>
+            <Modal.Card.Title>Run: {run_id}</Modal.Card.Title>
+          </Modal.Card.Head>
+          <Modal.Card.Body>
+            Run status: {status} <br />
+            Total expected number of test: {test_number} <br />
+            Additional outputs:
+            <Section style={{
+              backgroundColor: 'white',
+              color: 'black',
+              fontFamily: 'monospace',
+              fontSize: '0.8em',
+              overflow: 'auto',
+              whiteSpace: 'pre',
+            }}>
+              <pre>
+                <code>
+                  {return_message}
+                </code>
+              </pre>
+            </Section>
+          </Modal.Card.Body>
+        </Modal.Card>
+      </Modal>
+    );
+  }
+}
+
+RunDetails.propTypes = {
+  return_code: PropTypes.number,
+  return_message: PropTypes.string,
+}
+
+
+export class RunSummary extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      show_message: false,
+      show_modal: false,
     };
     this.handleOpen = this.handleOpen.bind(this);
     this.handleClose = this.handleClose.bind(this);
   }
 
   handleOpen() {
-    this.setState({ show_message: true });
+    this.setState({ show_modal: true });
   }
 
   handleClose() {
-    this.setState({ show_message: false });
+    this.setState({ show_modal: false });
   }
 
   render() {
-    const { return_code, return_message } = this.props;
-    return (
-      <React.Fragment>
-        <span style={{ color: "red", cursor: "pointer" }} onClick={this.handleOpen}>
-          [{return_code}]
+    let header = [
+      <Moment fromNow withTitle>
+        {this.props.date_started}
+      </Moment>
+    ];
+
+    let duration = null;
+    if (this.props.status === "finished") {
+      let converted_duration = convert(this.props.total_duration);
+      duration = (
+        <span>
+          {converted_duration.value.toFixed(converted_duration.precision)}{" "}
+          {converted_duration.unit} <br />
         </span>
-        <Modal
-          closeOnBlur={true}
-          show={this.state.show_message}
-          showClose={false}
-          onClose={this.handleClose}
-        >
-          <Modal.Content style={{ display: 'flex', width: '90%' }}>
-            <Section style={{
-              backgroundColor: 'white',
-              color: 'black',
-              flexGrow: '1',
-              fontFamily: 'monospace',
-              fontSize: '0.8em',
-              overflow: 'auto',
-              whiteSpace: 'pre',
-            }}>
-              {return_message}
-            </Section>
-          </Modal.Content>
-        </Modal>
-      </React.Fragment>
+      );
+
+      if (this.props.return_code !== undefined && this.props.return_code !== 0) {
+        header.push(<span style={{ color: "red" }}>[{this.props.return_code}]</span>);
+      }
+    }
+
+    let count = null;
+    if (this.props.status !== "starting") {
+      count = (
+        <span>
+          {this.props.done} / {this.props.test_number}
+        </span>
+      );
+    }
+    console.log("State", this.state.show_modal);
+
+    return (
+      <Message>
+        <Message.Header key={this.props.run_id + "header"} style={{ cursor: "pointer" }} onClick={this.handleOpen} >{header}</Message.Header >
+        <Message.Body>
+          {this.props.status} {this.props.duration !== null && <span>in {this.props.duration}</span>}
+          {count}
+          <Progress max={this.props.test_number || 0} value={this.props.done || 0} />
+        </Message.Body>
+        <RunDetails show={this.state.show_modal} handle_close={this.handleClose} run_id={this.props.run_id} done={this.props.done} test_number={this.props.test_number} status={this.props.status} return_message={this.props.return_message}></RunDetails>
+      </Message >
     );
   }
 }
 
-ReturnCode.propTypes = {
-  return_code: PropTypes.number,
-  return_message: PropTypes.string,
-};
-
-export const RunDetails = ({
-  date_started,
-  run_id,
-  status,
-  done,
-  test_number,
-  return_code,
-  return_message,
-  total_duration
-}) => {
-  let header = [
-    <Moment fromNow withTitle>
-      {date_started}
-    </Moment>
-  ];
-
-  let duration = null;
-  if (status === "finished") {
-    let converted_duration = convert(total_duration);
-    duration = (
-      <span>
-        {converted_duration.value.toFixed(converted_duration.precision)}{" "}
-        {converted_duration.unit} <br />
-      </span>
-    );
-
-    if (return_code !== undefined && return_code !== 0) {
-      header.push(<ReturnCode return_code={return_code} return_message={return_message} />);
-    }
-  }
-
-  let count = null;
-  if (status !== "starting") {
-    count = (
-      <span>
-        {done} / {test_number}
-      </span>
-    );
-  }
-
-  return (
-    <Message>
-      <Message.Header>{header}</Message.Header>
-      <Message.Body>
-        {status} {duration !== null && <span>in {duration}</span>}
-        {count}
-        <Progress max={test_number || 0} value={done || 0} />
-      </Message.Body>
-    </Message>
-  );
-};
-
-RunDetails.propTypes = {
+RunSummary.propTypes = {
   date_started: PropTypes.object,
   run_id: PropTypes.string,
   status: PropTypes.string,
@@ -134,7 +137,7 @@ export class RunsList extends React.Component {
     let runs = [];
     for (let run of _.reverse(_.values(this.props.runs))) {
       runs.push(
-        <RunDetails
+        <RunSummary
           key={run.run_id}
           date_started={run.date_started}
           run_id={run.run_id}
